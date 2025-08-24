137c61379825eaca881977e5f518ce922a3e269c
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2025-08-24 16:58:16 +0200

    ci

--- packages/typed-openapi/vitest.config.ts
+++ packages/typed-openapi/vitest.config.ts
@@ -1,11 +1,17 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from "vitest/config";
+import { defineConfig, configDefaults } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   test: {
     hideSkippedTests: true,
+    ...process.env["TEST_UNIT"] ? {
+      exclude: [
+        ...configDefaults.exclude,
+        '**\/tests\/integration-runtime-msw.test.ts',
+      ],
+    } : undefined,
     snapshotFormat: {
       escapeString: false,
     },
